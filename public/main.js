5714334c89bc6506d9b3f3369f30e8f9f5ab935c
Arpit Kulshrestha <Arpit_Kulshrestha@external.mckinsey.com>
2015-08-08 20:32:42 +0530

    mock the value

--- public/main.js
+++ public/main.js
@@ -437,14 +437,14 @@ $(function() {
             dataType: 'jsonp',
             success: function(location) {
                 // example where I update content on the page.
-                window.localStorage.setItem("city", "Delhi");
+                window.localStorage.setItem("city", location.city);
                 window.localStorage.setItem("region-code",location.region_code);
-                window.localStorage.setItem("region-name","NCR");
+                window.localStorage.setItem("region-name",location.region_name);
                 window.localStorage.setItem("areacode",location.areacode);
                 window.localStorage.setItem("ip",location.ip);
                 window.localStorage.setItem("longitude",location.longitude);
                 window.localStorage.setItem("latitude",location.latitude);
-                window.localStorage.setItem("country-name","India");
+                window.localStorage.setItem("country-name",location.country_name);
                 window.localStorage.setItem("country-code",location.country_code);
             }
         });
@@ -502,7 +502,7 @@ $(function() {
   };
     peopleNearMe = function(){
 
-            var newRoom = window.localStorage.getItem("city");
+            var newRoom = "Delhi"//window.localStorage.getItem("city");
             if (newRoom == ""){
                 newRoom = window.localStorage.getItem("country-name");
             }
@@ -568,7 +568,7 @@ $(function() {
     };
 
     peopleNotSoNearMe = function(){
-        var newRoom = window.localStorage.getItem("region-name");
+        var newRoom = "Delhi"//window.localStorage.getItem("region-name");
         if (newRoom == ""){
             newRoom = window.localStorage.getItem("country-name");
         }
